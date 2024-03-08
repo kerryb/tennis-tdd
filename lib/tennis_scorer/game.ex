45076ff91fe806ba3788b452e9ffa7ad29657670
Kerry Buckley <kerryjbuckley@gmail.com>
2024-03-08 20:01:37 +0000

    Move score calculation to Game

--- lib/tennis_scorer/game.ex
+++ lib/tennis_scorer/game.ex
@@ -1,4 +1,8 @@
 defmodule TennisScorer.Game do
   @moduledoc false
   defstruct [:player_one_name, :player_two_name, :player_one_score, :player_two_score]
+
+  def score(game) do
+    "#{game.player_one_name} #{game.player_one_score} – #{game.player_two_score} #{game.player_two_name}"
+  end
 end
