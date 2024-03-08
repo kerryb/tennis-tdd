d9aa93a1921d073a2d0be550de84d2a0946233fb
Kerry Buckley <kerryjbuckley@gmail.com>
2024-03-08 20:36:20 +0000

    Implement proper scoring

--- lib/tennis_scorer/game.ex
+++ lib/tennis_scorer/game.ex
@@ -3,6 +3,43 @@ defmodule TennisScorer.Game do
   defstruct [:player_one_name, :player_two_name, :player_one_score, :player_two_score]
 
   def score(game) do
-    "#{game.player_one_name} #{game.player_one_score} – #{game.player_two_score} #{game.player_two_name}"
+    cond do
+      game.player_one_score > 3 and game.player_one_score - game.player_two_score > 1 ->
+        game(game.player_one_name)
+
+      game.player_two_score > 3 and game.player_two_score - game.player_one_score > 1 ->
+        game(game.player_two_name)
+
+      game.player_one_score > 3 and game.player_one_score > game.player_two_score ->
+        advantage(game.player_one_name)
+
+      game.player_two_score > 3 and game.player_two_score > game.player_one_score ->
+        advantage(game.player_two_name)
+
+      game.player_one_score > 3 ->
+        deuce()
+
+      game.player_one_score == game.player_two_score ->
+        tied(game.player_one_score)
+
+      true ->
+        normal_score(game.player_one_score, game.player_two_score)
+    end
   end
+
+  defp game(name), do: "Game #{name}"
+  defp advantage(name), do: "Advantage #{name}"
+
+  defp deuce, do: "Deuce"
+
+  defp tied(score), do: String.capitalize("#{score_for(score)} all")
+
+  defp normal_score(player_one_score, player_two_score) do
+    String.capitalize("#{score_for(player_one_score)} #{score_for(player_two_score)}")
+  end
+
+  defp score_for(0), do: "love"
+  defp score_for(1), do: "15"
+  defp score_for(2), do: "30"
+  defp score_for(3), do: "40"
 end
---
+++ test/tennis_scorer/game_test.exs
@@ -0,0 +1,72 @@
+defmodule TennisScorer.GameTest do
+  use ExUnit.Case
+
+  alias TennisScorer.Game
+
+  describe "TennisScorer.Game.score/1" do
+    test "returns ‘Love all’ for 0 – 0" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 0, player_two_score: 0}
+      assert Game.score(game) == "Love all"
+    end
+
+    test "returns ‘Love 15’ for 0 – 1" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 0, player_two_score: 1}
+      assert Game.score(game) == "Love 15"
+    end
+
+    test "returns ‘15 love’ for 1 – 0" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 1, player_two_score: 0}
+      assert Game.score(game) == "15 love"
+    end
+
+    test "returns ‘15 30’ for 1 – 2" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 1, player_two_score: 2}
+      assert Game.score(game) == "15 30"
+    end
+
+    test "returns ‘40 30’ for 3 – 2" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 3, player_two_score: 2}
+      assert Game.score(game) == "40 30"
+    end
+
+    test "returns ‘15 all’ for 1 – 1" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 1, player_two_score: 1}
+      assert Game.score(game) == "15 all"
+    end
+
+    test "returns ‘Advantage <player one>’ for 4 – 3" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 4, player_two_score: 3}
+      assert Game.score(game) == "Advantage Alice"
+    end
+
+    test "continues to return advantage while player one is one point ahead" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 6, player_two_score: 5}
+      assert Game.score(game) == "Advantage Alice"
+    end
+
+    test "returns ‘Game <player one>’ when player one is two points ahead and > 3 points" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 4, player_two_score: 2}
+      assert Game.score(game) == "Game Alice"
+    end
+
+    test "returns ‘Advantage <player two>’ for 3 – 4" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 3, player_two_score: 4}
+      assert Game.score(game) == "Advantage Bob"
+    end
+
+    test "continues to return advantage while player two is one point ahead" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 5, player_two_score: 6}
+      assert Game.score(game) == "Advantage Bob"
+    end
+
+    test "returns ‘Deuce’ when players are tied on > 3 points" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 6, player_two_score: 6}
+      assert Game.score(game) == "Deuce"
+    end
+
+    test "returns ‘Game <player two>’ when player two is two points ahead and > 3 points" do
+      game = %Game{player_one_name: "Alice", player_two_name: "Bob", player_one_score: 2, player_two_score: 4}
+      assert Game.score(game) == "Game Bob"
+    end
+  end
+end
