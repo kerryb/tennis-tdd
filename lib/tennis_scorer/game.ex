18d33fa12e1acf8e8909ab0ad84be3b8c6bcc242
Kerry Buckley <kerryjbuckley@gmail.com>
2024-03-09 14:44:08 +0000

    Move functions to Game

--- lib/tennis_scorer/game.ex
+++ lib/tennis_scorer/game.ex
@@ -5,4 +5,14 @@ defmodule TennisScorer.Game do
   def new do
     %__MODULE__{player_one_name: "Player one", player_two_name: "Player two", player_one_score: 0, player_two_score: 0}
   end
+
+  def award_point_to_player_one(game), do: Map.update!(game, :player_one_score, &(&1 + 1))
+
+  def award_point_to_player_two(game), do: Map.update!(game, :player_two_score, &(&1 + 1))
+
+  def reset_scores(game), do: %{game | player_one_score: 0, player_two_score: 0}
+
+  def change_player_one_name(game, name), do: %{game | player_one_name: name}
+
+  def change_player_two_name(game, name), do: %{game | player_two_name: name}
 end
--- test/tennis_scorer/game_test.exs
+++ test/tennis_scorer/game_test.exs
@@ -20,4 +20,44 @@ defmodule TennisScorer.GameTest do
       assert Game.new().player_two_score == 0
     end
   end
+
+  describe "TennisScorer.Game.award_point_to_player_one/1" do
+    test "increments player one’s score" do
+      game = Game.new()
+      game = Game.award_point_to_player_one(game)
+      assert game.player_one_score == 1
+    end
+  end
+
+  describe "TennisScorer.Game.award_point_to_player_two/1" do
+    test "increments player two’s score" do
+      game = Game.new()
+      game = Game.award_point_to_player_two(game)
+      assert game.player_two_score == 1
+    end
+  end
+
+  describe "TennisScorer.Game.reset_scores/1" do
+    test "Resets both players’ scores" do
+      game = %Game{player_one_score: 1, player_two_score: 2}
+      game = Game.reset_scores(game)
+      assert %{player_one_score: 0, player_two_score: 0} = game
+    end
+  end
+
+  describe "TennisScorer.Game.change_player_one_name/2" do
+    test "Updates player one’s name" do
+      game = Game.new()
+      game = Game.change_player_one_name(game, "Alice")
+      assert %{player_one_name: "Alice"} = game
+    end
+  end
+
+  describe "TennisScorer.Game.change_player_two_name/2" do
+    test "Updates player two’s name" do
+      game = Game.new()
+      game = Game.change_player_two_name(game, "Bob")
+      assert %{player_two_name: "Bob"} = game
+    end
+  end
 end
