1c71b959221dde97f80d252fbca6b81d4cb15520
Kerry Buckley <kerryjbuckley@gmail.com>
2024-03-09 17:38:49 +0000

    Extract Game struct

---
+++ lib/tennis_scorer/game.ex
@@ -0,0 +1,16 @@
+defmodule TennisScorer.Game do
+  @moduledoc false
+  defstruct [:player_one_name, :player_two_name, :player_one_score, :player_two_score]
+
+  def new, do: %{player_one_name: "Player one", player_two_name: "Player two", player_one_score: 0, player_two_score: 0}
+
+  def award_point_to_player_one(game), do: Map.update!(game, :player_one_score, &(&1 + 1))
+
+  def award_point_to_player_two(game), do: Map.update!(game, :player_two_score, &(&1 + 1))
+
+  def reset_scores(game), do: %{game | player_one_score: 0, player_two_score: 0}
+
+  def change_player_one_name(game, name), do: Map.put(game, :player_one_name, name)
+
+  def change_player_two_name(game, name), do: Map.put(game, :player_two_name, name)
+end
