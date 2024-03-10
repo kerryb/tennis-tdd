1e276d3de5245008b53c554e7db5dba965a86773
Kerry Buckley <kerryjbuckley@gmail.com>
2024-03-10 19:41:12 +0000

    Give variables shorter names
    Every second saved typing/autocompleting is going to help!

--- lib/tennis_scorer_web/live/scoreboard_live.ex
+++ lib/tennis_scorer_web/live/scoreboard_live.ex
@@ -8,31 +8,29 @@ defmodule TennisScorerWeb.ScoreboardLive do
   def mount(_params, _session, socket) do
     {:ok,
      socket
-     |> assign(
-       game: %{player_one_name: "Player one", player_two_name: "Player two", player_one_score: 0, player_two_score: 0}
-     )
+     |> assign(game: %{name_1: "Player one", name_2: "Player two", score_1: 0, score_2: 0})
      |> build_form()}
   end
 
   @impl LiveView
-  def handle_event("point-to-player-one", _unsigned_params, socket) do
-    {:noreply, update(socket, :game, &award_point_to_player_one/1)}
+  def handle_event("point-to-player-1", _unsigned_params, socket) do
+    {:noreply, update(socket, :game, &award_point_to_player_1/1)}
   end
 
-  def handle_event("point-to-player-two", _unsigned_params, socket) do
-    {:noreply, update(socket, :game, &award_point_to_player_two/1)}
+  def handle_event("point-to-player-2", _unsigned_params, socket) do
+    {:noreply, update(socket, :game, &award_point_to_player_2/1)}
   end
 
   def handle_event("reset-scores", _unsigned_params, socket) do
     {:noreply, update(socket, :game, &reset_scores/1)}
   end
 
-  def handle_event("change", %{"_target" => ["player_one_name"], "player_one_name" => name}, socket) do
-    {:noreply, socket |> update(:game, &change_player_one_name(&1, name)) |> build_form()}
+  def handle_event("change", %{"_target" => ["name_1"], "name_1" => name}, socket) do
+    {:noreply, socket |> update(:game, &change_name_1(&1, name)) |> build_form()}
   end
 
-  def handle_event("change", %{"_target" => ["player_two_name"], "player_two_name" => name}, socket) do
-    {:noreply, socket |> update(:game, &change_player_two_name(&1, name)) |> build_form()}
+  def handle_event("change", %{"_target" => ["name_2"], "name_2" => name}, socket) do
+    {:noreply, socket |> update(:game, &change_name_2(&1, name)) |> build_form()}
   end
 
   def handle_event(_event, _unsigned_params, socket), do: {:noreply, socket}
@@ -41,19 +39,19 @@ defmodule TennisScorerWeb.ScoreboardLive do
     assign(socket,
       form:
         to_form(%{
-          "player_one_name" => socket.assigns.game.player_one_name,
-          "player_two_name" => socket.assigns.game.player_two_name
+          "name_1" => socket.assigns.game.name_1,
+          "name_2" => socket.assigns.game.name_2
         })
     )
   end
 
-  defp award_point_to_player_one(game), do: Map.update!(game, :player_one_score, &(&1 + 1))
+  defp award_point_to_player_1(game), do: Map.update!(game, :score_1, &(&1 + 1))
 
-  defp award_point_to_player_two(game), do: Map.update!(game, :player_two_score, &(&1 + 1))
+  defp award_point_to_player_2(game), do: Map.update!(game, :score_2, &(&1 + 1))
 
-  defp reset_scores(game), do: %{game | player_one_score: 0, player_two_score: 0}
+  defp reset_scores(game), do: %{game | score_1: 0, score_2: 0}
 
-  defp change_player_one_name(game, name), do: Map.put(game, :player_one_name, name)
+  defp change_name_1(game, name), do: Map.put(game, :name_1, name)
 
-  defp change_player_two_name(game, name), do: Map.put(game, :player_two_name, name)
+  defp change_name_2(game, name), do: Map.put(game, :name_2, name)
 end
--- test/tennis_scorer_web/live/scoreboard_live_test.exs
+++ test/tennis_scorer_web/live/scoreboard_live_test.exs
@@ -8,25 +8,25 @@ defmodule TennisScorerWeb.ScoreboardLiveTest do
   describe "TennisScorerWeb.ScoreboardLive" do
     test "allows player names to be set", %{conn: conn} do
       {:ok, view, _html} = live(conn, "/")
-      change_player_one_name(view, "Alice")
-      assert_player_one_name(view, "Alice")
+      change_name_1(view, "Alice")
+      assert_name_1(view, "Alice")
 
-      change_player_two_name(view, "Bob")
-      assert_player_two_name(view, "Bob")
+      change_name_2(view, "Bob")
+      assert_name_2(view, "Bob")
     end
 
     test "tracks the score as points are scored", %{conn: conn} do
       {:ok, view, _html} = live(conn, "/")
-      award_point_to_player_one(view)
+      award_point_to_player_1(view)
       assert_score(view, "1 – 0")
-      award_point_to_player_two(view)
+      award_point_to_player_2(view)
       assert_score(view, "1 – 1")
     end
 
     test "allows the scores to be reset", %{conn: conn} do
       {:ok, view, _html} = live(conn, "/")
-      award_point_to_player_one(view)
-      award_point_to_player_two(view)
+      award_point_to_player_1(view)
+      award_point_to_player_2(view)
       reset_scores(view)
       assert_score(view, "0 – 0")
     end
@@ -37,32 +37,32 @@ defmodule TennisScorerWeb.ScoreboardLiveTest do
     end
   end
 
-  defp change_player_one_name(view, name) do
-    view |> element("form") |> render_change(%{"_target" => ["player_one_name"], "player_one_name" => name})
+  defp change_name_1(view, name) do
+    view |> element("form") |> render_change(%{"_target" => ["name_1"], "name_1" => name})
   end
 
-  defp change_player_two_name(view, name) do
-    view |> element("form") |> render_change(%{"_target" => ["player_two_name"], "player_two_name" => name})
+  defp change_name_2(view, name) do
+    view |> element("form") |> render_change(%{"_target" => ["name_2"], "name_2" => name})
   end
 
-  defp award_point_to_player_one(view) do
-    view |> element("#point_to_player_one") |> render_click()
+  defp award_point_to_player_1(view) do
+    view |> element("#point_to_player_1") |> render_click()
   end
 
-  defp award_point_to_player_two(view) do
-    view |> element("#point_to_player_two") |> render_click()
+  defp award_point_to_player_2(view) do
+    view |> element("#point_to_player_2") |> render_click()
   end
 
   defp reset_scores(view) do
     view |> element("#reset_scores") |> render_click()
   end
 
-  defp assert_player_one_name(view, name) do
-    assert view |> element("#player_one_name[value='#{name}']") |> has_element?()
+  defp assert_name_1(view, name) do
+    assert view |> element("#name_1[value='#{name}']") |> has_element?()
   end
 
-  defp assert_player_two_name(view, name) do
-    assert view |> element("#player_two_name[value='#{name}']") |> has_element?()
+  defp assert_name_2(view, name) do
+    assert view |> element("#name_2[value='#{name}']") |> has_element?()
   end
 
   defp assert_score(view, score) do
