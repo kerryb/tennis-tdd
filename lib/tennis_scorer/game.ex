588bfb28306914156619cf6bea9e94e07789d8b5
Kerry Buckley <kerryjbuckley@gmail.com>
2024-03-12 12:12:26 +0000

    Add Game struct

---
+++ lib/tennis_scorer/game.ex
@@ -0,0 +1,16 @@
+defmodule TennisScorer.Game do
+  @moduledoc false
+  defstruct [:name_1, :name_2, :score_1, :score_2]
+
+  def new, do: %__MODULE__{name_1: "Player one", name_2: "Player two", score_1: 0, score_2: 0}
+
+  def award_point_to_player_1(game), do: Map.update!(game, :score_1, &(&1 + 1))
+
+  def award_point_to_player_2(game), do: Map.update!(game, :score_2, &(&1 + 1))
+
+  def reset_scores(game), do: %{game | score_1: 0, score_2: 0}
+
+  def change_name_1(game, name), do: Map.put(game, :name_1, name)
+
+  def change_name_2(game, name), do: Map.put(game, :name_2, name)
+end
