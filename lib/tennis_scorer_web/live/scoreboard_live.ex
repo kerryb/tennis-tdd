73bd43689548301d4e7e198774c958dcca2d95c5
Kerry Buckley <kerryjbuckley@gmail.com>
2025-09-05 11:28:54 +0100

    Add unused game map to LiveView assigns

--- lib/tennis_scorer_web/live/scoreboard_live.ex
+++ lib/tennis_scorer_web/live/scoreboard_live.ex
@@ -8,6 +8,12 @@ defmodule TennisScorerWeb.ScoreboardLive do
   def mount(_params, _session, socket) do
     {:ok,
      assign(socket,
+       game: %{
+         name_1: "Player one",
+         name_2: "Player two",
+         score_1: 0,
+         score_2: 0
+       },
        name_1: "Player one",
        name_2: "Player two",
        score_1: 0,
