512930cacabd9fd53c5a85188d7b399df320c713
Kerry Buckley <kerryjbuckley@gmail.com>
2025-09-05 14:54:55 +0100

    Implement Game.score_player_1/1 and score_player_2/1

--- lib/tennis_scorer/game.ex
+++ lib/tennis_scorer/game.ex
@@ -4,4 +4,12 @@ defmodule TennisScorer.Game do
             name_2: "Player two",
             score_1: 0,
             score_2: 0
+
+  def score_player_1(game) do
+    Map.update!(game, :score_1, fn score -> score + 1 end)
+  end
+
+  def score_player_2(game) do
+    Map.update!(game, :score_2, fn score -> score + 1 end)
+  end
 end
--- test/tennis_scorer/game_test.exs
+++ test/tennis_scorer/game_test.exs
@@ -1,9 +1,25 @@
 defmodule TennisScorer.GameTest do
   use ExUnit.Case, async: true
 
+  alias TennisScorer.Game
+
   describe "TennisScorer.Game" do
     test "initialises to default names and zero scores" do
-      assert %{name_1: "Player one", name_2: "Player two", score_1: 0, score_2: 0} = %TennisScorer.Game{}
+      assert %{name_1: "Player one", name_2: "Player two", score_1: 0, score_2: 0} = %Game{}
+    end
+  end
+
+  describe "TennisScorer.Game.score_player_1/1" do
+    test "increments player one’s score" do
+      game = Game.score_player_1(%Game{score_1: 1})
+      assert game.score_1 == 2
+    end
+  end
+
+  describe "TennisScorer.Game.score_player_2/1" do
+    test "increments player two’s score" do
+      game = Game.score_player_2(%Game{score_2: 1})
+      assert game.score_2 == 2
     end
   end
 end
